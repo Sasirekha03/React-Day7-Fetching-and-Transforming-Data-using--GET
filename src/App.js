2e53a7e34fa7349e1eb0fd47263f975c685aacf5
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-05 09:53:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {  BrowserRouter,Routes, Route } from 'react-router-dom';
+import BookList from './Booklist'
+import Destination from './Destination'
+import Home from './Home';
+import About from './About';
+ import Contact from './Contact';
+import Naving from './Naving';
+import RegistrationForm from './RegistrationForm';
+//import SubmittedForm from './SubmittedForm';
+//import './App.css';
+
+const App = () => {
+  return (
+<BookList/>
+
+   
+  );
+};
+
+export default App;

2fcab5eebdd83e00eb091601ab498521f9bd2cd2
Sasirekha03 <108916484+Sasirekha03@users.noreply.github.com>
2023-04-06 06:24:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
 import React from 'react';
-import {  BrowserRouter,Routes, Route } from 'react-router-dom';
-import BookList from './Booklist'
-import Destination from './Destination'
-import Home from './Home';
-import About from './About';
- import Contact from './Contact';
-import Naving from './Naving';
-import RegistrationForm from './RegistrationForm';
-//import SubmittedForm from './SubmittedForm';
-//import './App.css';
+
+import Books from './Books'
+
 
 const App = () => {
   return (
-<BookList/>
+<Books/>
 
    
   );
